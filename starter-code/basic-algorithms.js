9c39aa33f863570c986d4a2b91684e95288ffc7d
Isak La Fleur Engdahl <isak.engdahl@gmail.com>
2017-10-09 22:04:44 +0200

    Update basic-algorithms.js

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,93 +1,91 @@
 // Authors: Isak La Fleur & Andreas Karpasitis
-// Names and Input
-
-console.log("I'm ready!");
-
-const hacker1 = "Isak"
-console.log("The driver's name is " + hacker1);
+// Name and inputs
+const hacker1 = "Isak";
 const hacker2 = prompt("What is the name of the navigator?");
-console.log("The navigator's name is " + hacker2);
 
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
 
-//Conditionals
-// Compare the length of the driver's and navigator's name
+// Conditionals
 if (hacker1.length > hacker2.length) {
-  console.log("The Driver has the longest name, it has " + hacker1.length + " characters");
+  console.log(
+    `The Driver has the longest name, it has ${hacker1.length} characters`,
+  );
 } else if (hacker1.length < hacker2.length) {
-  console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters");
+  console.log(
+    `Yo, navigator got the longest name, it has ${hacker2.length} characters`,
+  );
 } else {
-  console.log("wow, you both got equally long names, " + hacker1.length + " characters!!");
+  console.log(`wow, your both got equally long names, ${hacker1.length}`);
 }
 
 // Loops
-// Print the Driver's name with spaces and capitalized
-var stringDriver = "";
-
-for (var i=0; i < hacker1.length; i++) {
-  stringDriver += hacker1[i].toUpperCase() + ' ';
+let driverNameReverse = "";
+for (let i = 0; i < hacker1.length; i++) {
+  driverNameReverse += hacker1[i].toUpperCase() + " ";
 }
-console.log(stringDriver);
+console.log(driverNameReverse);
 
-// Reverse the navigator's name
-console.log(reverse(hacker2));
+let navigatorNameReverse = hacker2
+  .split("")
+  .reverse()
+  .join("");
+console.log(navigatorNameReverse);
 
+// Lexicographic order
 
-// Check lexicographic order of the string
-if (hacker1 < hacker2) {
+if (hacker1 > hacker2) {
   console.log("The driver's name goes first");
-} else if (hacker1 > hacker2) {
+} else if (hacker1 < hacker2) {
   console.log("Yo, the navigator goes first definitely");
 } else {
   console.log("What?! You both got the same name?");
 }
 
-// Bonus Time
-// Check if it's a Palindrome.
+// Bonus Palindrome
+const stringArray = [
+  "A man, a plan, a canal, Panama!",
+  "Amor, Roma",
+  "race car",
+  "stack cats",
+  "step on no pets",
+  "taco cat",
+  "put it up",
+  "Was it a car or a cat I saw?",
+  "No 'x' in Nixon",
+  "This is not an palindrome!!!!",
+];
 
-// Ask user for string
-var string = prompt("Enter a new string please!");
-var re = /[^A-Za-z0–9]/g;
-var stringPalindrome = string.toLowerCase().replace(re, '');
-
-// print out the palindrome 
-// console.log(stringPalindrome);
-
-// Reverse the string
-var stringPalindromeReverse = reverse(stringPalindrome);
-
-// Check if string is a Palindrome
-if (stringPalindrome === stringPalindromeReverse) {
-  console.log("Its a Palindrome");
-} else {
-  console.log("Its not a Palindrome");
+function palindrome(string) {
+  const cleanString = string.toLowerCase().replace(/\W+/g, "");
+  const palindrome = cleanString
+    .split("")
+    .reverse()
+    .join("");
+  cleanString === palindrome
+    ? console.log("YES, it is a plaindrome")
+    : console.log("NO, it is not a palindrome");
 }
 
-
-// Lorem ipsum generator
-
-var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis mauris ut eleifend pharetra. Aliquam dictum metus non quam posuere, eu vulputate arcu pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc risus purus, malesuada luctus luctus id, posuere sollicitudin libero. Proin at mauris magna. Sed nunc ante, consectetur eget orci eget, posuere congue ipsum. Vestibulum semper porttitor ex, sit amet bibendum ex interdum tempus. Etiam venenatis accumsan velit a convallis. Etiam ut facilisis diam, tincidunt venenatis mi. Nam accumsan enim sagittis dolor lobortis, in fermentum quam accumsan. Vestibulum ex lectus, aliquet vel leo eu, gravida rhoncus erat. Cras sed imperdiet dolor. Duis eu faucibus tortor, nec rhoncus magna.\n Aenean ac iaculis risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent est nulla, sodales at ornare eu, maximus ac lectus. Vivamus bibendum lacus sit amet pretium mollis. Vestibulum convallis urna id lorem mattis convallis. Nunc non magna luctus, accumsan massa eget, efficitur tortor. Pellentesque tempus hendrerit viverra. Nullam ultricies ut felis nec molestie. Praesent sit amet tempus justo. Mauris in elementum lorem, non laoreet eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent hendrerit et diam sed sagittis. Cras at massa dolor. Suspendisse nec nunc risus\n Morbi augue urna, congue eu ex eget, bibendum elementum magna. Nullam tempus viverra ex. Aliquam id ultricies dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque fermentum enim vel auctor molestie. Proin ut sem non enim dignissim mollis. Cras vitae purus eu urna vestibulum efficitur. Mauris non lacus sollicitudin, fringilla justo eu, imperdiet arcu. Morbi suscipit non elit et tempor."
-console.log(lorem);
-
-// Split string into an array
-var loremArray = lorem.split(" ");
-
-// Print out number of words
-var wordCount = loremArray.length;
-console.log("In the lorem ipsum text there are " + wordCount + " words!");
-
-// Find the word et and count how many times it appears
-var counter = 0;
-
-for (var i = 0; i < loremArray.length; i++) {
-  if (loremArray[i] === "et") {
-    counter++;
-  }
+function checkPalindrome(array) {
+  array.forEach(item => palindrome(item));
 }
 
-console.log("The word et was found: " + counter + " times");
+checkPalindrome(stringArray);
 
+// lorem ipsum generator
 
-// Functions
-function reverse(s){
-    return s.split("").reverse().join("");
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis tincidunt enim. Phasellus vitae nunc vehicula, ultrices ante non, efficitur risus. Ut tristique lorem sit amet feugiat suscipit. Maecenas imperdiet mattis augue, pharetra varius nulla elementum ut. Mauris pulvinar pretium nulla, nec viverra ex tempor et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam in lacus et augue euismod commodo feugiat ac nunc. Integer vehicula lobortis auctor. Fusce pharetra odio vitae orci ornare efficitur. Nunc vulputate gravida lorem id tincidunt. Maecenas tincidunt neque ac sodales semper. Donec vulputate luctus purus, et porttitor urna interdum eu. Nulla consectetur et massa ac interdum. Phasellus ut facilisis sem. Sed in pellentesque dolor. Nam gravida tristique quam, quis laoreet felis varius eu. Duis in massa nisl. Sed at posuere urna, vel tristique diam. Donec euismod ipsum sit amet iaculis semper. Nam dapibus nisl lectus, eget pretium velit molestie sit amet. Morbi dictum sem neque, sit amet lacinia risus sagittis vitae. Donec et nisi vitae sem condimentum hendrerit non nec dui. Donec sit amet mi non dui maximus molestie at at neque. Maecenas at congue dolor, vel ultrices justo. Ut eleifend porta suscipit. In hac habitasse platea dictumst. Fusce lectus lectus, placerat vel maximus ut, fringilla ac sapien. Pellentesque id tincidunt lacus. Integer tincidunt libero at diam aliquam, non eleifend lectus interdum. Vestibulum vestibulum, nisi non vulputate sodales, urna dui convallis purus, sed luctus nibh nunc in libero. Ut consectetur finibus dolor, vitae elementum diam.";
+
+function countWordsInString(string) {
+  return string.replace(/\W+/g, " ").split(" ").length;
 }
+
+function countSpecificWordInString(string, wordToFind) {
+  re = /et/g;
+  return string.match(re).length;
+}
+console.log(countSpecificWordInString(lorem, "et"));
+
+console.log("Number of words in string: ", countWordsInString(lorem));
